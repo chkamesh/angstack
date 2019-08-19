a8b289f6e330190943a4fd525d0ce8ecf3410bf3
kameshchitimoju <chitimoju.kamesh@gmail.com>
2019-08-19 18:46:47 +0530

    1 file modified, 8 added

---
+++ src/app/ltmenu/ltmenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ltmenu',
+  templateUrl: './ltmenu.component.html',
+  styleUrls: ['./ltmenu.component.css']
+})
+export class LtmenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
