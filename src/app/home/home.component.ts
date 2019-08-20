5f5a1922e118a21a44819c2e92668f6b45949c10
kameshchitimoju <chitimoju.kamesh@gmail.com>
2019-08-20 18:31:07 +0530

    1 file modified, 1 added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private name: string = 'Hi'
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+  }
+  public save() {
+    console.log(this.name+""+this.route.url);
+    
+    this.router.navigate([{outlets: {primary: this.route.url }}]);
   }
 
 }
