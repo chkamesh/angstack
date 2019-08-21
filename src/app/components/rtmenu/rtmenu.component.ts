308519240516f41091b404092064e3ccfdf1ad2b
kameshchitimoju <chitimoju.kamesh@gmail.com>
2019-08-21 12:07:03 +0530

    1 file modified, 4 added

---
+++ src/app/components/rtmenu/rtmenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rtmenu',
+  templateUrl: './rtmenu.component.html',
+  styleUrls: ['./rtmenu.component.css']
+})
+export class RtmenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
