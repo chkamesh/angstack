188998bb5ac343a9ced47706d6779142a05b483b
kameshchitimoju <chitimoju.kamesh@gmail.com>
2019-08-21 16:35:44 +0530

    3 files modified

--- src/app/directives/sample.directive.ts
+++ src/app/directives/sample.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appSample]'
 })
 export class SampleDirective {
 
-  constructor() { }
+  constructor(private el:ElementRef) { 
+    el.nativeElement.style.backgroundColor='#bbbbbb'
+  }
 
 }
